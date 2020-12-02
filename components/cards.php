f67cd0f1febd752753e53a9908308bfe34d5d06c
Philippe Paris <contacto@philippeparis.net>
2020-12-02 13:57:19 -0300

    update style

--- components/cards.php
+++ components/cards.php
@@ -1,15 +1,18 @@
 <section class="marvel-cards">
+    <hr>
     <div class="container">
 
-        <h1 class="text-center marvel-cards__title">Marvel</h1>
-
+        <h1 class="text-center marvel-cards__title mt-3 mb-3">Marvel</h1>
+    </div>
+    <hr>
+    <div class="container mt-5">
         <p id="message" class="text-center marvel-cards__message"></p>
 
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-md-6">
-                <input id="mySearchField" name="search" placeholder="Buscar.." type="text">
-                <button id="mySearchButton">Buscar</button>
-                <button id="myCleanButton">Limpiar</button>
+                <input class="mr-1" id="mySearchField" name="search" placeholder="Buscar.." type="text">
+                <button class="btn btn--load mr-1" id="mySearchButton">Buscar</button>
+                <button class="btn btn--load mr-1" id="myCleanButton">Limpiar</button>
             </div>
             <div class="col-md-6 text-right">
                 <div id="myResponseArea"></div>
@@ -17,14 +20,14 @@
         </div>
 
         <div class="row justify-content-center">
-                <div id="myContentArea"></div>
+            <div id="myContentArea"></div>
         </div>
 
 
-        <div id="marvel-container" class="marvel-container">
+        <div id="marvel-container" class="marvel-container mt-5 mb-5">
         </div>
         <div class="text-center mt-4">
-            <a href="#" id="loadMore" class="btn btn--load">Cargar más</a>
+            <a href="#" id="loadMore" class="btn btn--load mb-5">Cargar más</a>
         </div>
     </div>
 </section>
--- layout/header.php
+++ layout/header.php
@@ -7,7 +7,7 @@
 
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
 		aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
+		<span class="navbar-toggler-icon material-icons">menu</span>
 	</button>
 
 	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
